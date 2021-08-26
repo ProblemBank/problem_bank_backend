2e81d8d022afe026974f9abdae9b0a7d825eeb67
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2021-08-27 01:24:28 +0430

    fixed settings

--- karsoogh/urls.py
+++ karsoogh/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
-from karsoogh import settings
+from django.conf import settings
 
 urlpatterns = \
     [
