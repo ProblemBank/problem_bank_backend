a761a0b42ba9105331816b9c3cb1a62f23eadde5
erfanmotnt <moeini.erfan@gmail.com>
2021-09-05 00:00:31 +0430

    start app problembank

--- karsoogh/settings/base.py
+++ karsoogh/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Account',
     'Game',
+    'problembank',
     'corsheaders',
     'rest_framework',
     'auction.apps.AuctionConfig',
---
+++ problembank/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProblembankConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'problembank'
