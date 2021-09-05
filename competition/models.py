f87b85a714477a0f6434a7c886cba46479d6380b
erfanmotnt <moeini.erfan@gmail.com>
2021-09-05 21:56:56 +0430

    final models for bank and simple model for compatition

--- competition/models.py
+++ competition/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from Account.models import User
 from model_utils.managers import InheritanceManager
 from django.utils import timezone
-from problembank import Problem, ShortAnswer, ShortAnswerSubmit
+from problembank.models import ShortAnswerProblem, ShortAnswer, ShortAnswerSubmit
 
 
 
-class GamingShortAnswerProblem(Problem):
-    answer = models.OneToOneField(ShortAnswer, null=True, on_delete=models.SET_NULL, unique=True,
-                                   related_name='problem', verbose_name='پاسخ صحیح')
+class GamingShortAnswerProblem(ShortAnswerProblem):
     cost = models.IntegerField(default=0, verbose_name='هزینه‌ی دریافت') 
     reward = models.IntegerField(default=0, verbose_name='پاداش حل‌کردن')
 
@@ -22,15 +20,15 @@ class Game(models.Model):
 
     maximum_number_of_received_problem_per_subject = \
         models.IntegerField(default=6,
-                            verbose_name='حداکثر تعداد سوالاتی که یک بازیکن در کل مسابقه از یک مبحث می‌تواند داشته باشد')
-
+                            verbose_name='حداکثر تعداد سوالاتی که یک بازیکن در کل مسابقه از یک دسته می‌تواند داشته باشد')
+    
     def __str__(self):
         return self.title
 
 class Player(models.Model):
     name = models.CharField(max_length=50, verbose_name='نام بازیکن')
-    users = models.ManyToManyField(User, verbose_name='کاربر(ان)')
-    game = models.ForeignKey(to=Game, on_delete=models.PROTECT, verbose_name='بازی')
+    users = models.ManyToManyField(User, verbose_name='کاربر(ان)', related_name='game_plays')
+    game = models.ForeignKey(to=Game, on_delete=models.PROTECT, verbose_name='بازی', related_name='players')
     score = models.IntegerField(default=0, verbose_name='امتیاز')
 
     def __str__(self):
--- karsoogh/settings/base.py
+++ karsoogh/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Account',
     'Game',
+    'competition',
     'problembank',
     'corsheaders',
     'rest_framework',
