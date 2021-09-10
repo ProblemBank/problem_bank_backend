67cdc219e3e9fc07a670180fd40be6d023c5eb5c
erfanmotnt <moeini.erfan@gmail.com>
2021-09-10 16:09:35 +0430

    add

--- problembank/forms.py
+++ problembank/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from problembank.widget import MoratabEditor
+# from problembank.widget import MoratabEditor
 
 class QuestionForm(forms.ModelForm):
-    text_tmp1 = forms.CharField(widget=MoratabEditor, required=False)
-    text_tmp2 = forms.CharField(widget=MoratabEditor, required=False)
+    text_tmp1 = forms.CharField(required=False)
+    text_tmp2 = forms.CharField(required=False)
     
